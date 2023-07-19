0094009c7781e847008c21b846127ad3779ca6b9
RohitKewat <rohit554514@gmail.com>
2023-07-19 21:04:39 +0530

    initial push

---
+++ src/components/navigation.jsx
@@ -0,0 +1,23 @@
+import {Container,Navbar,Nav} from 'react-bootstrap';
+
+
+const Navigation = () => {
+
+    return (
+
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home"> Web Connect Solutions </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link> Services </Nav.Link>
+                        <Nav.Link>About Us </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Navigation 
