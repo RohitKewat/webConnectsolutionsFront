25054210e3015d79b80c6f517faea17d566fed70
Rohit Kewat <122372268+RohitKewat@users.noreply.github.com>
2023-07-23 07:09:46 +0000

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,23 +1,20 @@
-import {Container,Navbar,Nav} from 'react-bootstrap';
-
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 const Navigation = () => {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home"> Web Connect Solutions </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link> Services </Nav.Link>
+            <Nav.Link>About Us </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-    return (
-
-        <>
-            <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                    <Navbar.Brand href="#home"> Web Connect Solutions </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link> Services </Nav.Link>
-                        <Nav.Link>About Us </Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
-    )
-}
-
-export default Navigation 
+export default Navigation;
